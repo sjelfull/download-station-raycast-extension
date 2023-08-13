73baab94222d3be18e3d17759ed27d63532c52ef
Fred Carlsen <fred@sjelfull.no>
2023-08-13 20:22:57 +0200

    fixup! initial commit
    Signed-off-by: Fred Carlsen <fred@sjelfull.no>

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { ActionPanel, Detail, List, Action, getPreferenceValues } from "@raycast/api";
+import { ActionPanel, Detail, List, Action, getPreferenceValues, Icon } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import React, { useEffect, useMemo, useState } from "react";
 import { SessionInfo, TasksEntity, TasksResponse } from "./types";
@@ -16,6 +16,7 @@ export default function Command() {
   const [showingDetail, setShowingDetail] = useState(false);
   const { hostname, username, password } = getPreferenceValues<Preferences.Index>();
   const endpoint = `${hostname}/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${username}&passwd=${password}&session=DownloadStation&format=sid`;
+  // TODO When returned from cache, the JSON is a string. Maybe the parser callback option should be able to handle that?
   const { data: sessionInfo, isLoading, revalidate } = useFetch<SessionInfo>(endpoint);
   const [sid, setSid] = useState<string | undefined>(parse(sessionInfo)?.data?.sid);
 
@@ -32,15 +33,15 @@ export default function Command() {
 
   const [list, setList] = useState<TasksEntity[]>(parse(taskList)?.data?.tasks || []);
   const [statuses, setStatuses] = useState<string[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<string>('all');
+  const [selectedStatus, setSelectedStatus] = useState<string>("all");
   const [searchText, setSearchText] = useState("");
   const [filteredList, filterList] = useState<Record<string, TasksEntity[]>>(groupByStatus(list) || {});
   const onToggleDetail = () => {
     setShowingDetail(!showingDetail);
-  }
+  };
   const onStatusFilterChange = (newStatus: string) => {
     setSelectedStatus(newStatus);
-  }
+  };
 
   // useEffect(() => {
   //   console.log(sessionInfo, 'sessionInfo on mount');
@@ -56,16 +57,16 @@ export default function Command() {
   //   }, [])
 
   const filterCallback = (task: TasksEntity) => {
-    const matchStatus = selectedStatus === 'all' || task.status === selectedStatus;
-    const matchTitle = searchText === '' || task.title.toLowerCase().includes(searchText);
+    const matchStatus = selectedStatus === "all" || task.status === selectedStatus;
+    const matchTitle = searchText === "" || task.title.toLowerCase().includes(searchText);
 
     return matchTitle && matchStatus;
-  }
+  };
 
   useEffect(() => {
     const grouped = groupByStatus(list?.filter(filterCallback) || []);
     filterList(grouped);
-    setStatuses(['all', ...Object.keys(grouped)]);
+    setStatuses(["all", ...Object.keys(grouped)]);
   }, [list]);
 
   useEffect(() => {
@@ -143,7 +144,11 @@ function StatusFilterDropdown(props: { statuses: string[]; onStatusFilterChange:
     >
       <List.Dropdown.Section title="Status">
         {statuses.map((status) => (
-          <List.Dropdown.Item key={`status-${status}`} title={`${status.substring(0, 1).toLocaleUpperCase()}${status.substring(1)}`} value={status} />
+          <List.Dropdown.Item
+            key={`status-${status}`}
+            title={`${status.substring(0, 1).toLocaleUpperCase()}${status.substring(1)}`}
+            value={status}
+          />
         ))}
       </List.Dropdown.Section>
     </List.Dropdown>
@@ -151,14 +156,22 @@ function StatusFilterDropdown(props: { statuses: string[]; onStatusFilterChange:
 }
 
 function formatValue(value: any): string {
-  if (typeof value === 'number') {
-      // Assuming value in bytes for filesize. Adjust as needed.
-      return bytesToSize(value); 
+  if (typeof value === "number") {
+    // Assuming value in bytes for filesize. Adjust as needed.
+    return bytesToSize(value);
   }
   return value.toString();
 }
 
-function DownloadEntry({task, onToggleDetail, showingDetail}: {task: TasksEntity, onToggleDetail: () => void, showingDetail: boolean}) {
+function DownloadEntry({
+  task,
+  onToggleDetail,
+  showingDetail,
+}: {
+  task: TasksEntity;
+  onToggleDetail: () => void;
+  showingDetail: boolean;
+}) {
   const formatMetadata = useMemo(() => {
     const { detail, transfer } = task.additional;
 
@@ -170,64 +183,105 @@ function DownloadEntry({task, onToggleDetail, showingDetail}: {task: TasksEntity
     // });
 
     metadataArray.push({
-        title: "Size Downloaded",
-        value: formatValue(transfer.size_downloaded),
+      title: "Downloaded",
+      value: `${formatValue(transfer.size_downloaded)} / ${formatValue(task.size)}`,
     });
 
     metadataArray.push({
-        title: "Progress",
-        value: calculateDownloadProgress(transfer.size_downloaded, task.size),
+      title: "Progress",
+      value: calculateDownloadProgress(transfer.size_downloaded, task.size),
     });
 
     metadataArray.push({
-        title: "Size Uploaded",
-        value: formatValue(transfer.size_uploaded),
+      title: "Size Uploaded",
+      value: formatValue(transfer.size_uploaded),
     });
 
     metadataArray.push({
-        title: "Speed",
-        value: `${formatSpeed(transfer.speed_download)} (D) / ${formatSpeed(transfer.speed_upload)} (U)`,
+      title: "Speed",
+      value: `${formatSpeed(transfer.speed_download)} (D) / ${formatSpeed(transfer.speed_upload)} (U)`,
     });
 
     metadataArray.push({
-        title: "Peers/Seeders",
-        value: `${detail.connected_peers} / ${detail.connected_seeders}`,
+      title: "Peers/Seeders",
+      value: `${detail.connected_peers} / ${detail.connected_seeders}`,
     });
 
+    // metadataArray.push({
+    //     title: "File Size",
+    //     value: formatValue(task.size),
+    // });
+
     metadataArray.push({
-        title: "File Size",
-        value: formatValue(task.size),
+      title: "Destination",
+      value: task.additional.detail.destination,
     });
 
     return metadataArray;
+  }, [task]);
 
-}, [task]);
-  
   const props: Partial<List.Item.Props> = showingDetail
-            ? {
-                detail: (
-                  <List.Item.Detail
-                    metadata={
-                      <List.Item.Detail.Metadata>
-                      {formatMetadata.map((item, index) => (
-                          <React.Fragment key={index}>
-                              <List.Item.Detail.Metadata.Label title={item.title} text={item.value} />
-                              {index !== formatMetadata.length - 1 && <List.Item.Detail.Metadata.Separator />}
-                          </React.Fragment>
-                      ))}
-                  </List.Item.Detail.Metadata>
-                    }
-                    markdown={`\n\n${JSON.stringify(task.additional, null, 2)}`}
-                  />
-                ),
-              }
-            : { accessories: [{ text: [bytesToSize(task.size)].join(' ') }] };
-          
-  return <List.Item title={task.title} subtitle={task.status} {...props}
-  actions={
-      <ActionPanel>
-        <Action title="Toggle Detail" onAction={() => onToggleDetail()} />
-      </ActionPanel>
-    }
-  />
+    ? {
+        detail: (
+          <List.Item.Detail
+            metadata={
+              <List.Item.Detail.Metadata>
+                {formatMetadata.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <List.Item.Detail.Metadata.Label title={item.title} text={item.value} />
+                    {index !== formatMetadata.length - 1 && <List.Item.Detail.Metadata.Separator />}
+                  </React.Fragment>
+                ))}
+              </List.Item.Detail.Metadata>
+            }
+            markdown={`\n\n`}
+          />
+        ),
+      }
+    : {
+        accessories: [
+          {
+            text: [
+              // bytesToSize(task.size),
+              `${calculateDownloadProgress(task.additional.transfer.size_downloaded, task.size)} of ${bytesToSize(
+                task.size
+              )}`,
+            ].join(" "),
+          },
+        ],
+      };
+
+  return (
+    <List.Item
+      icon={statusToIcon(task.status)}
+      title={task.title}
+      subtitle={task.status}
+      {...props}
+      actions={
+        <ActionPanel>
+          <Action title="Toggle Detail" onAction={() => onToggleDetail()} />
+        </ActionPanel>
+      }
+    />
+  );
+}
+
+
+function statusToIcon(status: string) {
+  switch (status) {
+    case "downloading":
+      return Icon.Download;
+    case "paused":
+      return Icon.Pause;
+    case "finished":
+      return Icon.Checkmark;
+    case "seeding":
+      return Icon.Play;
+    case "waiting":
+      return Icon.QuestionMark;
+    case "error":
+      return Icon.Warning;
+    default:
+      return Icon.Play;
+  }
 }
--- src/utilities.ts
+++ src/utilities.ts
@@ -2,43 +2,62 @@ import { TasksEntity } from "./types";
 
 const BASE_URL = "http://YOUR_SYNOLOGY_IP:PORT/webapi/";
 
+const statusPriorities: { [status: string]: number } = {
+  all: 1,
+  downloading: 2,
+  paused: 3,
+  completed: 4,
+  error: 5,
+  waiting: 5,
+};
+
 export function groupByStatus(downloads: TasksEntity[]) {
-    return downloads.reduce<Record<string, TasksEntity[]>>((acc, download) => {
-        const status = download.status
-      if (!acc[status]) {
-        acc[status] = [];
-      }
-      acc[status].push(download);
-      
+  const grouped = downloads.reduce<Record<string, TasksEntity[]>>((acc, download) => {
+    const status = download.status;
+    if (!acc[status]) {
+      acc[status] = [];
+    }
+    acc[status].push(download);
+
+    return acc;
+  }, {});
+
+  return Object.entries(grouped)
+    .sort(([statusA], [statusB]) => {
+      const priorityA = statusPriorities[statusA];
+      const priorityB = statusPriorities[statusB];
+      return priorityA - priorityB;
+    })
+    .reduce<Record<string, TasksEntity[]>>((acc, [key, value]) => {
+      acc[key] = value;
       return acc;
     }, {});
-  };
+}
 
-  export function bytesToSize(bytes: number, precision: number = 0): string {
-    if (bytes < 0) return "Invalid Size";  // or handle as per your need
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    if (bytes === 0) return '0 Bytes';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString(), 10);
-    if (i < 0) return '0 Bytes';
-    return (bytes / Math.pow(1024, i)).toFixed(precision) + ' ' + sizes[i];
+export function bytesToSize(bytes: number, precision: number = 0): string {
+  if (bytes < 0) return "Invalid Size"; // or handle as per your need
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  if (bytes === 0) return "0 Bytes";
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString(), 10);
+  if (i < 0) return "0 Bytes";
+  return (bytes / Math.pow(1024, i)).toFixed(precision) + " " + sizes[i];
 }
 
 export function formatSpeed(bytesPerSecond: number): string {
-    const speeds = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s'];
+  const speeds = ["B/s", "KB/s", "MB/s", "GB/s", "TB/s"];
 
-    if (bytesPerSecond === 0) return '0 B/s';
-    const i = parseInt(Math.floor(Math.log(bytesPerSecond) / Math.log(1024)).toString(), 10);
-    if (i < 0) return '0 B/s';  // In case bytesPerSecond is between 0 and 1.
-    return (bytesPerSecond / Math.pow(1024, i)).toFixed(2) + ' ' + speeds[i];
+  if (bytesPerSecond === 0) return "0 B/s";
+  const i = parseInt(Math.floor(Math.log(bytesPerSecond) / Math.log(1024)).toString(), 10);
+  if (i < 0) return "0 B/s"; // In case bytesPerSecond is between 0 and 1.
+  return (bytesPerSecond / Math.pow(1024, i)).toFixed(2) + " " + speeds[i];
 }
 
 export function calculateDownloadProgress(downloaded: number, fileSize: number): string {
-    if (downloaded === 0 || fileSize === 0) return `0%`;
+  if (downloaded === 0 || fileSize === 0) return `0%`;
 
-    const progress = (downloaded / fileSize) * 100;
-    return `${parseFloat(progress.toFixed(2)).toString()}%`;  // You can adjust the precision as needed
+  const progress = (downloaded / fileSize) * 100;
+  return `${parseFloat(progress.toFixed(2)).toString()}%`; // You can adjust the precision as needed
 }
-
 
 // // Authenticate and get the SID
 // export const authenticate = async (account, password) => {
